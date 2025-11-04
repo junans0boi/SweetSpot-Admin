eb2e490b53b17bc2c27358956420c977d5e0f99f
junans0boi <junans0boi@gmail.com>
2025-11-04 13:46:34 +0900

    Complete admin backend implementation

---
+++ backend/src/test/java/com/hollywood/sweetspotadmin/SweetspotadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollywood.sweetspotadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetSpotAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("✅ Context loaded successfully (Skipping DB connection).");
+    }
+}
